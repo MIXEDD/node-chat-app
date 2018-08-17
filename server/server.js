25967464f0debe13b5c226a049a35d6a9e121bf4
Maksimilian <maxas81@gmail.com>
2018-08-17 13:13:05 +0300

    Emit newMessage on createMessgaeg

--- server/server.js
+++ server/server.js
@@ -5,23 +5,23 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'Hey.',
-    createAt: 123
-  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage ',message);
+    io.emit('newMessage',{
+        from:message.from,
+        text:message.text,
+        createAt:new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
